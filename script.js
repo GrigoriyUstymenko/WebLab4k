379dda1ba03ac2a196073b6fd66b3ecd0fd3aa8e
GrigoriyUstymenko <gromgoriy@gmail.com>
2022-01-12 05:56:26 +0200

    Started worksheet

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 'use strict';
 
-const insertItem = document.querySelector('.insert-item');
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-  const result = await fetch('https://web-lab3k.herokuapp.com/v1/graphql', {
-    method: 'POST',
-    body: JSON.stringify({
-      query: operationsDoc,
-      variables: variables,
-      operationName: operationName,
-    }),
-  });
+const playButton = document.querySelector('.play-button');
+const work = document.querySelector('.work');
 
-  return await result.json();
-}
-
-
-const addItem = `mutation MyMutation($content: String = "", $title: String = "") {
-  insert_accordion_one(object: {content: $content, title: $title}) {
-    id
-  }
-}`;
-
-const sendItem = async event => {
-  event.preventDefault();
-
-  const entries = Array.from(insertItem.elements).map(e => [e.name, e.value]);
-  const data = Object.fromEntries(entries);
-  await fetchGraphQL(addItem, 'MyMutation', {
-    title: data.title,
-    content: data.content,
-  });
-};
-
-insertItem.addEventListener('submit', sendItem);
+playButton.addEventListener('click', (event) => {
+  event.target.disabled = true;
+  work.style.display = 'block';
+})
